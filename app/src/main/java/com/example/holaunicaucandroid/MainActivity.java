2337bd5fdf81ad9101c486050d324bd712f73257
tjhonatan <tjhonatan@unicauca.edu.co>
2019-09-26 19:36:13 -0500

    Withput date and time update

--- app/src/main/java/com/example/holaunicaucandroid/MainActivity.java
+++ app/src/main/java/com/example/holaunicaucandroid/MainActivity.java
@@ -26,6 +26,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
-        updateTime();
+
+        //updateTime();
     }
 }
